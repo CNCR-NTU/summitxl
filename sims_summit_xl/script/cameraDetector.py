9002dd75b69a8fda8c750e9b2e00a0e1a3892925
Enzo-M <enzomena@gmail.com>
2017-05-22 12:39:55 +0100

    [add] camera detector with real robot

--- sims_summit_xl/script/cameraDetector.py
+++ sims_summit_xl/script/cameraDetector.py
@@ -6,8 +6,7 @@ import numpy as np
 import cv2
 from cv_bridge import CvBridge, CvBridgeError # bridge for converting ROS Messages to OpenCV encoding and reverse
 from sensor_msgs.msg import Image # encoding type of ROS Images
-from sims_summit_xl.msg import pos as Position_Msg
-from matplotlib import pyplot as plt
+from sims_summit_xl.msg import pos as Position_Msg # definition of Position_Msg (msg/pos.msg)
 
 np.seterr(all='raise')  
 
@@ -15,10 +14,12 @@ class cameraDetector:
 	'''Initializing bridge subscriber and Publisher and getting data 
 	from camera and synchronizing rgb and depth frames'''
 	def __init__(self):
+		
+		
 		self.bridge = CvBridge()
-		RGB_img = message_filters.Subscriber('/orbbec_astra/rgb/image_raw', Image)
-		depth_img = message_filters.Subscriber('/orbbec_astra/depth/image_raw', Image)
-		self.TimeSync = message_filters.TimeSynchronizer([RGB_img,depth_img],10)
+		rgb_img = message_filters.Subscriber('/orbbec_astra/rgb/image_raw', Image)# /!\ if using the simulation, topic name needs to be change /!\
+		depth_img = message_filters.Subscriber('/orbbec_astra/depth_registered/image_raw', Image) # /!\ if using the simulation, topic name needs to be change /!\
+		self.TimeSync = message_filters.ApproximateTimeSynchronizer([rgb_img,depth_img], 10, 0.5)
 		self.TimeSync.registerCallback(self.tracking) # callback function tracking (sending image data towards it)
 		self.image_publisher = rospy.Publisher('SIMS/image_topic',Image, queue_size=3)
 		self.pos_publisher = rospy.Publisher('SIMS/position_data',Position_Msg,queue_size=3)
@@ -33,32 +34,39 @@ class cameraDetector:
 
 	'''Main function of tracking'''
 	def tracking(self,rgb_data,depth_data):
+		
+		#rospy.logwarn(depth_data.encoding) #To know the encoding of the depth image (16UC1 (greyscale) or 32FC1 (distance))
 
 		# getting the images dimensions
 		self.image_height = rgb_data.height
 		self.image_width = rgb_data.width
-
 		# transforming ROS Messages data to OpenCV data 
 		try:
 			cv_rgb = self.bridge.imgmsg_to_cv2(rgb_data,desired_encoding='rgb8')
 			cv_depth = self.bridge.imgmsg_to_cv2(depth_data,desired_encoding = 'passthrough')
 		except CvBridgeError as e :
 			print(e)
+			rospy.logwarn('empty depth array. all depth values are nan')
+
 		
+		
+		#transform image format from BGR(opencv) to RGB (normal)
+		cv_rgb = cv2.cvtColor(cv_rgb, cv2.COLOR_BGR2RGB)
+
 		# define range of blue color in HSV
-		lower = np.array([0, 100,100])
-		upper = np.array([40, 255, 255])
+		lower = np.array([80, 100,100])
+		upper = np.array([110, 255, 255])
 		
-		# blur image and convert to HSV color encoding
-		blurred = cv2.GaussianBlur(cv_rgb,(9,9),0)
+		# blur image and 40convert to HSV color encoding
+		blurred = cv2.GaussianBlur(cv_rgb,(11,11),0)
 		hsv = cv2.cvtColor(blurred,cv2.COLOR_BGR2HSV)
 		
-		# HSV image to get only blue colors
+		# HSV image to get only blue colors6
 		mask1 = cv2.inRange(hsv,lower,upper)
 		
 		# reducing the noise by eroding an then dilating, the iterations are arbitrary
-		mask2 = cv2.erode(mask1, None, iterations=3)
-		mask3 = cv2.dilate(mask2,None, iterations=3)
+		mask2 = cv2.erode(mask1, None, iterations=5)
+		mask3 = cv2.dilate(mask2,None, iterations=5)
 		
 		# find contours of the object
 		cnts = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
@@ -74,26 +82,22 @@ class cameraDetector:
 			pos = self.getPos_contour(cnt,cv_depth)
 			if self.checkPos(pos):
 				self.lastPos = pos 
-				# getting angles from pos
-				angleX = self.getAngleX(pos)
-				angleY = self.getAngleY(pos)
-				#creating the Position_Msg and publishing it
-				pos_Msg = Position_Msg(angleX,angleY,pos[1])
-				rospy.logwarn(pos_Msg)
-				self.pos_publisher.publish(pos_Msg)
+				self.publish_PosMsg(pos)
 			else:
 				rospy.logwarn('no plausible position found')
 
 
-
+			#drawing squares around detected objects.
 			rect = cv2.minAreaRect(cnt)
 			box = cv2.boxPoints(rect)
 			box = np.int0(box)
 			cv2.drawContours(cv_rgb,[box],0,(0,0,255),2)
 
-			
-			
-		#publishing on a topic which can be seen in rviz for example
+
+		#transform image format from RGB (normal) to BGR(opencv)
+		cv_rgb = cv2.cvtColor(cv_rgb, cv2.COLOR_BGR2RGB)
+
+		#publishing the image with the square(s) around detected object(s) on a topic which can be seen in rviz 
 		try:
 			self.image_publisher.publish(self.bridge.cv2_to_imgmsg(cv_rgb,rgb_data.encoding))
 		except CvBridgeError as e:
@@ -128,10 +132,10 @@ class cameraDetector:
 
 		# getting rid of all Nan values which are not useful
 		depthArray = depthObject[~np.isnan(depthObject)]
-		
+
 		# np.mean gives the average of the array elements
 		averageDistance = np.mean(depthArray)
-
+		
 		if len(depthArray) == 0:
 			rospy.logwarn('empty depth array. all depth values are nan')
 				
@@ -155,16 +159,20 @@ class cameraDetector:
 
 		if np.isnan(distance):
 			ret = False
+			rospy.logwarn('distance is nan')
 		
 		# testing the distance value
-		if abs(distance-Ldistance)>0.4:
+		if abs(distance-Ldistance)>0.25:
 			ret = False
+			rospy.logwarn('distance is too different')
 		
 		# testing the coordinates 
-		if abs(centerX-LcenterX)>(self.image_width/6):
+		if abs(centerX-LcenterX)>(self.image_width/4):
 			ret = False
-		if abs(centerY-LCenterY)>(self.image_height/6):
+			rospy.logwarn('centerX is too different')
+		if abs(centerY-LCenterY)>(self.image_height/4):
 			ret = False
+			rospy.logwarn('centerY is too different')
 
 		return ret
 
@@ -181,6 +189,15 @@ class cameraDetector:
 		displacement = 2*centerY/self.image_height-1
 		angleY = -1*np.arctan(displacement*self.tan_vertical)
 		return angleY
+
+	''' Publish the position of the object detected '''
+	def publish_PosMsg(self,pos):
+		# getting angles from pos
+		angleX = self.getAngleX(pos)
+		angleY = self.getAngleY(pos)
+		#creating the Position_Msg and publishing it
+		pos_Msg = Position_Msg(angleX,angleY,pos[1])
+		self.pos_publisher.publish(pos_Msg)
 
 		
 if __name__ == '__main__':
